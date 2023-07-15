a4a59501296d7e7bf0efcbdd053a57b19928c821
GOMATHISANKAR22 <117799775+GOMATHISANKAR22@users.noreply.github.com>
2023-07-15 06:42:55 +0100

    Update provider.tf

--- terraform-aws-serverless-static-wordpress-master/provider.tf
+++ terraform-aws-serverless-static-wordpress-master/provider.tf
@@ -1,16 +1,15 @@
 terraform {
-  required_version = ">= 0.15.1"
+  required_version = "> 0.15.1"
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      # https://github.com/hashicorp/terraform-provider-aws/blob/main/CHANGELOG.md
+      source                = "hashicorp/aws"
       version               = "~> 3.0"
       configuration_aliases = [aws.ue1]
     }
-    random = {
-      source = "hashicorp/random"
-      # https://github.com/hashicorp/terraform-provider-random/blob/main/CHANGELOG.md
-      version = "~> 3.1.0"
-    }
   }
+}
+
+provider "aws" {
+  alias   = "ue1"
+  region  = "us-east-1"
 }
