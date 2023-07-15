687d49727a883e95e89dbceb5bc887568fda1480
GOMATHISANKAR22 <117799775+GOMATHISANKAR22@users.noreply.github.com>
2023-07-15 06:29:09 +0100

    Update main.tf

--- terraform-aws-serverless-static-wordpress-master/modules/cloudfront/main.tf
+++ terraform-aws-serverless-static-wordpress-master/modules/cloudfront/main.tf
@@ -4,17 +4,17 @@ data "archive_file" "index_html" {
   output_path = "${path.module}/lambda_redirect/dst/index_html.zip"
 }
 
-#tfsec:ignore:AWS089
-resource "aws_cloudwatch_log_group" "object_redirect" {
-  name              = "/aws/lambda/${var.site_name}_redirect_index_html"
-  retention_in_days = 7
-}
+# #tfsec:ignore:AWS089
+# resource "aws_cloudwatch_log_group" "object_redirect" {
+#   name              = "/aws/lambda/${var.site_name}_redirect_index_html"
+#   retention_in_days = 7
+# }
 
-#tfsec:ignore:AWS089
-resource "aws_cloudwatch_log_group" "object_redirect_ue1_local" {
-  name              = "/aws/lambda/us-east-1.${var.site_name}_redirect_index_html"
-  retention_in_days = 7
-}
+# #tfsec:ignore:AWS089
+# resource "aws_cloudwatch_log_group" "object_redirect_ue1_local" {
+#   name              = "/aws/lambda/us-east-1.${var.site_name}_redirect_index_html"
+#   retention_in_days = 7
+# }
 
 # TODO: A solution to create/manage default log groups in all Edge Cache Regions
 #tfsec:ignore:AWS089
@@ -31,14 +31,14 @@ resource "aws_lambda_function" "object_redirect" {
   role             = aws_iam_role.lambda-edge.arn
   handler          = "index.handler"
   source_code_hash = data.archive_file.index_html.output_base64sha256
-  runtime          = "nodejs12.x"
+  runtime          = "nodejs18.x"
   publish          = true
   memory_size      = 128
   timeout          = 3
   depends_on = [
-    aws_cloudwatch_log_group.object_redirect,
+   # aws_cloudwatch_log_group.object_redirect,
     aws_cloudwatch_log_group.object_redirect_ue1,
-    aws_cloudwatch_log_group.object_redirect_ue1_local
+   # aws_cloudwatch_log_group.object_redirect_ue1_local
   ]
 }
 
