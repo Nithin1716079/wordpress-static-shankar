27c337f760f7f7695af529172a056f8bb784efda
GOMATHISANKAR22 <117799775+GOMATHISANKAR22@users.noreply.github.com>
2023-07-15 06:43:19 +0100

    Update outputs.tf

--- terraform-aws-serverless-static-wordpress-master/outputs.tf
+++ terraform-aws-serverless-static-wordpress-master/outputs.tf
@@ -1,7 +1,7 @@
-output "cloudfront_ssl_arn" {
-  value       = aws_acm_certificate.wordpress_site.arn
-  description = "The ARN of the ACM certificate used by CloudFront."
-}
+# output "cloudfront_ssl_arn" {
+#   value       = aws_acm_certificate.wordpress_site.arn
+#   description = "The ARN of the ACM certificate used by CloudFront."
+# }
 
 output "wordpress_ecr_repository" {
   value       = aws_ecr_repository.serverless_wordpress.name
